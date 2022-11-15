f450d00386a21e8adf11a9625f4bc3c57897f84b
Johnny_P <shinezl39@gmail.com>
2022-11-15 09:08:20 +0900

    redux practice

--- src/App.js
+++ src/App.js
@@ -7,12 +7,22 @@ import NewsSagaBox from './Components/NewsSagaBox';
 /* import News from './Components/News'; */
 
 function App() {
+  /* const [num, setNum] = useState(0);
+  
+  const increase = () => {
+    setNum(num + 1);
+  }
+  */
+
   return (
     <div className="App">
       <CounterBox></CounterBox>
       <NewsSagaBox></NewsSagaBox>
       {/* <Memo></Memo> */}
       {/* <News></News> */}
+
+      {/* <h1>{num}</h1>
+      <button onClick={increase()}></button> */}
     </div>
   );
 }
